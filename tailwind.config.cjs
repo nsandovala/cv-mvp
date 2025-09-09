b4faf34adbcc95ad0098f6f98fc11db48a5010c0
Nelson Sandoval <nandres.sandoval@gmail.com>
2025-09-08 22:43:38 -0300

    fix: package.json, scripts y .gitignore para Vercel

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter','ui-sans-serif','system-ui','Segoe UI','Roboto','Helvetica','Arial'],
-      },
-    },
-  },
-  plugins: [],
-};
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: { extend: {} },
+  plugins: []
+}
